8aa8324aefd877737ef188a72e50076c6b7417c2
Kelvin Francisco <127231395+saofranciscc@users.noreply.github.com>
2024-05-02 16:30:43 -0300

    Update Login.jsx

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -3,36 +3,26 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../api'
-
-// Importa o arquivo de estilo CSS
 import '../styler/Login.css'
 
-// Define o componente Login
 function Login() {
-  // Define estados para o nome de usuário e senha
+  
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  // Obtém a função de navegação do hook useNavigate
   const navigate = useNavigate()
 
-  // Função para lidar com o envio do formulário de login
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      // Faz uma solicitação POST para a API para obter o token de acesso
       const response = await api.post(`api/token/`, { username, password })
-      // Armazena o token de acesso no localStorage
       localStorage.setItem('token', response.data.access)
-      // Redireciona para a página de listagem de posts após o login bem-sucedido
       navigate('alunos/')
     } catch (error) {
-      // Registra qualquer erro ocorrido ao fazer login
       console.error('Erro ao fazer login:', error)
     }
   }
 
-  // Retorna a interface do componente Login
   return (
     <div className="login-container"> {/* Adiciona uma classe ao contêiner */}
       <h1>Login</h1>
@@ -47,5 +37,4 @@ function Login() {
   )
 }
 
-// Exporta o componente Login
 export default Login
